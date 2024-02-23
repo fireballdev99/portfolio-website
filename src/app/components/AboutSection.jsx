8ab38bb8f58c873062be252baaf4a4f1d65bff1d
Fireball.Dev <fireball.dev99@gmail.com>
2024-02-24 01:22:54 +0700

    First Commit Finish About Section

---
+++ src/app/components/AboutSection.jsx
@@ -0,0 +1,105 @@
+"use client";
+import Image from "next/image";
+import React, { useTransition, useState } from "react";
+import TabButton from "./TabButton";
+
+const TAB_DATA = [
+  {
+    title: "Skills",
+    id: "skills",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>NEXT</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MYSQL</li>
+        <li>Python</li>
+        <li>Flask</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Education",
+    id: "education",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Born to Dev</li>
+        <li>Rangsit University</li>
+      </ul>
+    ),
+  },
+  {
+    title: "Certifications",
+    id: "certifications",
+    content: (
+      <ul className="list-disc pl-2">
+        <li>Udemy Fullstack Developer Course</li>
+        <li>Udemy ReactJS Developer Course</li>
+      </ul>
+    ),
+  },
+];
+
+const AboutSection = () => {
+  const [tab, setTab] = useState("skills");
+  const [isPending, startTransition] = useTransition();
+
+  const handleTabChange = (id) => {
+    startTransition(() => {
+      setTab(id);
+    });
+  };
+  return (
+    <section className="text-white">
+      <div className="md:grid md:grid-cols-2 gap-8 items-center py-8 px-4 xl:gap-16 sm:py-16 xl:px-16">
+        <Image
+          src="/images/about-image.png"
+          className="rounded-lg"
+          width={500}
+          height={500}
+        />
+        <div className="mt-4 md:mt-0 text-left flex flex-col h-full">
+          <h2 className="text-4xl font-bold text-white mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I'm a dedicated full stack web developer enthusiastic about crafting
+            engaging and adaptable web solutions. Proficient in JavaScript,
+            React, Node.js, Express, MySQL, Python, HTML, and CSS, I thrive on
+            continuous learning and am eager to broaden my expertise.
+            Collaborating with others to innovate and build exceptional
+            applications is what excites me.
+          </p>
+          <div className="flex flex-row justify-start mt-8">
+            <TabButton
+              selectTab={() => handleTabChange("skills")}
+              active={tab === "skills"}
+            >
+              {" "}
+              Skills{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("education")}
+              active={tab === "education"}
+            >
+              {" "}
+              Education{" "}
+            </TabButton>
+            <TabButton
+              selectTab={() => handleTabChange("certifications")}
+              active={tab === "certifications"}
+            >
+              {" "}
+              Certifications{" "}
+            </TabButton>
+          </div>
+          <div className="mt-8">
+            {TAB_DATA.find((t) => t.id === tab).content}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default AboutSection;
